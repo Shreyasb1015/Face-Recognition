996261e705f36d656a0ad90e5e462c7a9f4a89bc
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-26 20:09:27 +0530

    updated recognize python script

--- recognize.py
+++ recognize.py
@@ -5,3 +5,10 @@ import numpy as np
 
 facecascade=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') #type:ignore
 
+with open('face-recognition/faces.pkl','rb') as w:
+    faces=pickle.load(w)
+
+with open('face-recognition/names.pkl','rb') as file:
+    labels=pickle.load(file)
+
+camera=cv2.VideoCapture(0)
