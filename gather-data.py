6667092307ac7e88ee897a912d2a978596437025
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-26 14:19:13 +0530

    converting frame to grayscale

--- gather-data.py
+++ gather-data.py
@@ -7,3 +7,21 @@ import pickle
 
 #Creating a list to store the frames of faces.
 face_data=[]  
+i=0
+
+camera=cv2.VideoCapture(0)
+
+#Loading the haarcascade
+facecascade = cv2.CascadeClassifier('Haarcascade/haarcascade_frontalface_default.xml')
+
+name=input('Enter Your name:')
+ret=True
+
+while(ret):
+    ret,frame=camera.read()
+    if ret == True:
+        #Converting the frame to grayscale image.
+        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        
+        
+
